39284cd4c6c76e8a5589f8db66d450754572dcc6
erickzanardo <erickzanardoo@gmail.com>
2015-01-30 16:05:23 -0200

    Basic Panel widget

--- assert.js
+++ assert.js
@@ -2,6 +2,10 @@ function Assert() {}
 
 Assert.prototype.eq = function(expected, actual, message) {
     console.log(message, expected == actual ? ' PASS ': ' FAIL ');
-}
+};
+
+Assert.prototype.isTrue = function(actual, message) {
+    console.log(message, actual ? ' PASS ': ' FAIL ');
+};
 
 module.exports = new Assert();
